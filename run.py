1704f3bcab883a73c75d6c06f1b297e5b36d5731
Valter Nepomuceno <valter.nep@gmail.com>
2020-12-04 23:31:47 +0000

    Add basic unit tests for `stats` module

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ print('__file__={0:<35} | __name__={1:<20} | __package__={2:<20}'.format(__file_
 
 import sys
 
-import gmail_fisher.authenticate
 from gmail_fisher.save_attachments import gmail_save_attachments
 from gmail_fisher.stats import plot_uber_eats_expenses
 
--- tests/test_save_attachments.py
+++ tests/test_save_attachments.py
@@ -1,5 +1,5 @@
 import unittest
-import gmail_fisher.save_attachments
+from gmail_fisher.save_attachments import get_payslip_filename
 
 
 class TestSaveAttachments(unittest.TestCase):
@@ -9,7 +9,7 @@ class TestSaveAttachments(unittest.TestCase):
     def test_get_payslip_filename(self):
         subject = 'Please note that the Payslip for the income earned in 8-2020 is attached.'
         expected = 'PaySlip_2020-8.pdf'
-        filename = gmail_fisher.save_attachments.get_payslip_filename(subject)
+        filename = get_payslip_filename(subject)
 
         assert filename == expected
 
